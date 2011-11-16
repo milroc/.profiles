a9fd90182769312f04daa592ea1f596501fe180f
Miles McCrocklin - work MBP <miles.mccrocklin@gmail.com>
2011-11-16 15:58:50 -0800

    remove sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#This install script is currently under the assumption you've installed zsh and oh-myzsh.
-
-ln -s my_cmd.zsh ~/.oh-my-zsh/custom/my_cmd.zsh
-ln -s .zshrc ~/.zshrc
-
-ln -s .bashrc ~/.bashrc
-ln -s .bash_profile ~/.bash_profile
-
